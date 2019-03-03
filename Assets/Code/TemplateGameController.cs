df1ba7ef86ade4864c928e9a2cbb8fba4745a01c
Nejc Anclin <anclin@MacBook-Pro-Anclin.local>
2019-03-03 13:50:32 +0100

    Initial commit.

---
+++ Assets/Code/TemplateGameController.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GameMachine;
+
+namespace TemplateGame {
+    
+    public class TemplateGameController : GameBase {
+    }
+
+}
